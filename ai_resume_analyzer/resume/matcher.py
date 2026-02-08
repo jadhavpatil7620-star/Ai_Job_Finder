704d1079a3b4f0e34b85aeb15cd327ae8f416e4b
Gopal Jadhav <jadhavpatil7620@gmail.com>
2026-02-08 20:03:48 +0530

    Adding all files which is implemented in files.

--- ai_resume_analyzer/resume/matcher.py
+++ ai_resume_analyzer/resume/matcher.py
@@ -2,9 +2,12 @@ def calculate_job_match(resume_skills,job):
     job_skills=[skill.name.lower() for skill in job.required_skills.all()]
     
     if not job_skills:
-        return 0
+        return 0,[],[]
     
-    matched=set(resume_skills).intersection(resume_skills)
+    resume_skills=[s.lower() for s in resume_skills]
+    
+    matched=sorted(set(resume_skills).intersection(job_skills))
+    missing=sorted(set(job_skills)-set(matched))
     match_percentage=int((len(matched)/len(job_skills))*100)
     
-    return match_percentage,matched
+    return match_percentage,matched,missing
